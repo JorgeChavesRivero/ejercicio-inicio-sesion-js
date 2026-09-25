55b2fa7a4cecac41dadf1c2ee28d28e1048584af
JorgeChavesRivero <jorgealexanderchavesrivero@gmail.com>
2026-09-24 20:08:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function validarAcceso() {
+    const USUARIOCORRECTO = `admin`;
+    const CONTRASENACORRECTA = `admin123`;
+    const INTENTOSMAXIMO = 3;
+    let intentos = 0;
+
+    while (intentos < INTENTOSMAXIMO) {
+        let usuarioPedido = prompt(`Ingrese su usuario`).toLowerCase();
+        let contrasenaPedida = prompt(`Ingrese su contraseña`);
+        
+        if (usuarioPedido === USUARIOCORRECTO && contrasenaPedida === CONTRASENACORRECTA) {
+            console.log(`¡Bienvenido al sistema!`);
+            intentos = INTENTOSMAXIMO;
+        } else {
+            intentos++;
+            console.log(`Datos incorrectos. Intento ${intentos} de ${INTENTOSMAXIMO}.`);
+            if (intentos===0) {
+                console.log(`Usuario bloqueado. Ha superado el número de intentos.`);
+            }
+        }
+    }
+}
+validarAcceso();
